b15ffac7363603be455155955ab3222c641e1031
udhos <udhos>
2023-04-16 20:36:15 -0300

    New chart.

--- cmd/sqs-to-sns/main.go
+++ cmd/sqs-to-sns/main.go
@@ -22,7 +22,7 @@ import (
 	"github.com/udhos/sqs-to-sns/sqsclient"
 )
 
-const version = "0.4.0"
+const version = "0.5.0"
 
 func getVersion(me string) string {
 	return fmt.Sprintf("%s version=%s runtime=%s boilerplate=%s GOOS=%s GOARCH=%s GOMAXPROCS=%d",
